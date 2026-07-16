f507cca6db907f5097fcfe1f94c15f7389b80d46
sparshtub <sparsh2110154@akgec.ac.in>
2026-07-16 22:12:01 +0530

    change to authcontext

--- frontend/components/AuthContext.tsx
+++ frontend/components/AuthContext.tsx
@@ -10,7 +10,7 @@ interface AuthContextType {
   isAuthenticated: boolean;
   loading: boolean;
 }
-
+// sparsh is good 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 export function AuthProvider({ children }: { children: React.ReactNode }) {
